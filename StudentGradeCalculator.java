700adfb6b524e3ced1f4371d19c0d80c1777ee41
Prasad-Mengane <159246900+Prasad-Mengane@users.noreply.github.com>
2025-02-22 13:29:07 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,56 @@
+package com.java.dev;
+
+import java.util.*;
+
+class Grade
+{
+    public void Marks()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter no of Subject: ");
+        int sub = sc.nextInt();
+
+        int sum = 0;
+        for(int i=1; i<=sub; i++)
+        {
+            System.out.print("Enter Subject "+i+" Marks (out of 100): ");
+            int marks = sc.nextInt();
+            sum += marks;
+        }
+
+        double avg = sum/sub;
+
+        char grade;
+        if(avg >= 80)
+        {
+            grade = 'A';
+        }
+        else if(avg >= 50 && avg <80)
+        {
+            grade = 'B';
+        }
+        else if(avg >= 35 && avg < 50)
+        {
+            grade = 'C';
+        }
+        else
+        {
+            grade = 'F';
+        }
+        System.out.print("\nTotal Marks: "+sum);
+        System.out.print("\nAverage Percentage: "+avg);
+        System.out.print("\nGrade: "+grade);
+        sc.close();
+    }
+}
+
+public class StudentGradeCalculator 
+{
+	public static void main(String[] args) 
+	{
+		Grade obj = new Grade();
+		obj.Marks();
+	}
+
+}
