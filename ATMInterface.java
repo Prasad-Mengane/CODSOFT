44b1d2cbb722cfba9ca9436f34223db9746c96e7
Prasad-Mengane <159246900+Prasad-Mengane@users.noreply.github.com>
2025-03-22 10:32:53 +0530

    Update ATMInterface.java

--- ATMInterface.java
+++ ATMInterface.java
@@ -1,67 +1,48 @@
 package com.java.dev;
-
 import java.util.*;
 
-class BankAccount
-{
+class BankAccount{
     private double balance;
-    public BankAccount(double initialbalance)
-    {
+    public BankAccount(double initialbalance){
         this.balance = initialbalance;
     }
-    public double getBalance()
-    {
+    public double getBalance(){
         return balance;
     }
-    public void deposit(double amount)
-    {
-        if(amount > 0)
-        {
+    public void deposit(double amount){
+        if(amount > 0){
             balance += amount;
             System.out.println("Amount Deposited Successfully");
         }
-        else
-        {
+        else{
             System.out.println("Invalid Amount");
         }
     }
-    public void withdraw(double amount)
-    {
-        if(amount > 0 && amount <= balance)
-        {
+    public void withdraw(double amount){
+        if(amount > 0 && amount <= balance){
             balance -= amount;
             System.out.println("Amount Withdraw Successfully");
         }
-        else
-        {
+        else{
             System.out.println("Insufficient Balance");
         }
     }
 }
-
-class ATM
-{
+class ATM{
     private BankAccount UserAccount;
-    public ATM(BankAccount account)
-    {
+    public ATM(BankAccount account){
         this.UserAccount = account;
     }
-
     Scanner sc = new Scanner(System.in);
-
-    public void displayMenu()
-    {
-        while(true)
-        {
+    public void displayMenu(){
+        while(true){
             System.out.println("ATM Menu");
             System.out.println("1. Check Balance");
             System.out.println("2. Withdraw");
             System.out.println("3. Deposit");
             System.out.print("Choose An Option: ");
-
             int choice = sc.nextInt();
-            switch(choice)
-            {
+            switch(choice){
                 case 1:
                     checkBalance();
                     break;
@@ -79,31 +60,24 @@ class ATM
         }
     }
 
-    private void checkBalance()
-    {
+    private void checkBalance(){
         System.out.println("Your Balance is: $"+UserAccount.getBalance());
     }
-    private void deposit()
-    {
+    private void deposit(){
         System.out.print("Enter The Amount: ");
         double amount = sc.nextDouble();
         UserAccount.deposit(amount);
     }
-    private void withdraw()
-    {
+    private void withdraw(){
         System.out.print("Enter The Amount: ");
         double amount = sc.nextDouble();
         UserAccount.withdraw(amount);
     }
 }
-
 public class ATMInterface {
-
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		BankAccount userAccount = new BankAccount(500.0);
 		ATM obj = new ATM(userAccount);
 		obj.displayMenu();
 	}
-
 }
