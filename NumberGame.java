3a9ab69a1981561d78434b56fc10e67429a6ec0f
Prasad-Mengane <159246900+Prasad-Mengane@users.noreply.github.com>
2025-03-22 10:28:41 +0530

    Update NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,70 +1,50 @@
 package com.java.dev;
-
 import java.util.*;
 
-class game
-{
+class game{
     Scanner sc = new Scanner(System.in);
     int round = 0;
     int win = 0;
 
-    public void playGame()
-    {
+    public void playGame(){
         boolean play = true;
-
-        while(play)
-        {
+        while(play){
             Random random = new Random();
             int num = random.nextInt(101);
             int attempts = 0;
             int maxAttempts = 5;
-    
             round++;
-
-            while(attempts < maxAttempts)
-            {
+            while(attempts < maxAttempts){
                 System.out.print("Enter Your Guess: ");
                 int guess = sc.nextInt();
                 attempts++;
         
-                if(guess<num)
-                {
+                if(guess<num){
                     System.out.println("No is low");
                 }
-                else if(guess>num)
-                {
+                else if(guess>num){
                     System.out.println("No is high");
                 }
-                else
-                {
+                else{
                     System.out.println("You get the no");
                     win++;
                     break;
                 }
             }
-
             System.out.print("You want to play again (yes/no): ");
             String playAgain = sc.next().trim().toLowerCase();
-
-            if(playAgain.equals("no"))
-            {
+            if(playAgain.equals("no")){
                 play = false;
             }
         }
-
         System.out.print("\nTotal Rounds: "+round);
         System.out.print("\nTotal Win: "+win);
         sc.close();
-        
     }
 }
-
-public class NumberGame 
-{
-	public static void main(String[] args) 
-	{
+public class NumberGame {
+	public static void main(String[] args) {
 		game obj = new game();
 		obj.playGame();
 	}
-
 }
